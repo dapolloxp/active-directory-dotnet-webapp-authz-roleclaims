09722eb3bea3fc32df626830717cf69ab42987b3
David Apolinar <david.apolinar@hotmail.com>
2017-03-14 15:09:38 -0400

    Added App Insights

--- WebApp-RoleClaims-DotNet/App_Start/FilterConfig.cs
+++ WebApp-RoleClaims-DotNet/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WebApp_RoleClaims_DotNet
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
